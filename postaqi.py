7d1a06f257c685ebcd0cc58d68d77b4fcf52b52d
Elam Day <elam.day.friedland@gmail.com>
2021-01-18 14:13:34 -0800

    complete, untested

--- postaqi.py
+++ postaqi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Periodically send bme280 data via POST request to a designated host"""
 import sys
 import time
@@ -7,17 +7,28 @@ import requests
 from busio import I2C
 import adafruit_bme280
 # Create library object using our Bus I2C port
-
-host = sys.argv[1]
-name = sys.argv[2]
+try:
+    config = open("config", "r")
+    host = config.readline()
+    name = config.readline()
+except Exception as e:
+    print(e)
+i2c = I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 
 # change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
+if len(sys.argv) == 2:
+    try:
+        print(requests.post(host, data={'id':1, "name":name, "temp":bme280.temperature, "humidity":bme280.humidity, "pressure":bme280.pressure, "altitude":bme280.altitude}))
+    except Exception as e:
+        print(e)
+
 
 while True:
     try:
         print(requests.post(host, data={'id':1, "name":name, "temp":bme280.temperature, "humidity":bme280.humidity, "pressure":bme280.pressure, "altitude":bme280.altitude}))
-    except:
-        print('frick')
-    time.sleep(30)
+    except Exception as e:
+        print(e)
+    time.sleep(60)
