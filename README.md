2995d22678403bf19461b1984c32a04731f47cc0
Elam Day <elam.day.friedland@gmail.com>
2021-01-18 15:29:54 -0800

    asdf

---
+++ README.md
@@ -0,0 +1,8 @@
+# Low Cost, Distributed, and Open Source Air Quality Sampling!
+
+## Welcome!
+Below are the links to all of the documents you need to get your very own distributed air quality sensor mesh set up!
+
+[Spin up a dashboard for your data with POST endpoints for data collection!](server.md)
+[Configure and host MySql database to store your data!](db.md)
+[Set up a Raspberry Pi to send data to your server!](sensor.md)
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # copyleft me (Elam) 2021; keep the software free, baby
 if [ "$(id -u)" != "0" ]; then
-echo "You must be the superuser to run this script :( try adding \"sudo\" at the begining of your command and rerun" >&2
-exit 1
+	echo "You must be the superuser to run this script :( try adding \"sudo\" at the begining of your command and rerun" >&2
+	exit 1
 fi
 i2cDeviceExists() {
 	devs=$(i2cdetect -y 1 | sed 1d | sed 's/^....//' | sed 's/--//g')
