80ad50f7abf0fafad4da11303805ff692c3986f9
Elam Day <elam.day.friedland@gmail.com>
2021-01-18 19:31:31 -0800

    a

--- postaqi.py
+++ postaqi.py
@@ -21,14 +21,14 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 bme280.sea_level_pressure = 1013.25
 if len(sys.argv) == 2:
     try:
-        print(requests.post(host, data={'id':1, "name":name, "temp":bme280.temperature, "humidity":bme280.humidity, "pressure":bme280.pressure, "altitude":bme280.altitude}))
+        print(requests.post(host, data={"name":name, "temp":bme280.temperature, "humidity":bme280.humidity, "pressure":bme280.pressure, "altitude":bme280.altitude}))
     except Exception as e:
         print(e)
 
 
 while True:
     try:
-        print(requests.post(host, data={'id':1, "name":name, "temp":bme280.temperature, "humidity":bme280.humidity, "pressure":bme280.pressure, "altitude":bme280.altitude}))
+        print(requests.post(host, data={"name":name, "temp":bme280.temperature, "humidity":bme280.humidity, "pressure":bme280.pressure, "altitude":bme280.altitude}))
     except Exception as e:
         print(e)
     time.sleep(60)
