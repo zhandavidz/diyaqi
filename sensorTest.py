3d8b24b1973c4d79c08fcd93c6de6ad9f9a036d2
Elam Day <elam.day.friedland@gmail.com>
2021-01-18 17:23:45 -0800

    oops forgot

---
+++ sensorTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Test the BME280 Sensor"""
+
+import sys
+import time
+import board
+from busio import I2C
+import adafruit_bme280
+
+i2c = I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+bme280.sea_level_pressure = 1013.25
+
+print(bme280.temperature)
