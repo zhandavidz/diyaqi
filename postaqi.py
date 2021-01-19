d635e2801fe896d993e0ea74642ac4df033923f2
Elam Day <elam.day.friedland@gmail.com>
2021-01-18 19:53:49 -0800

    whoops

--- postaqi.py
+++ postaqi.py
@@ -12,18 +12,20 @@ try:
     host = config.readline()
     name = config.readline()
 except Exception as e:
+    print(e)
+try:
+    i2c = I2C(board.SCL, board.SDA)
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+except:
     try:
         i2c = I2C(board.SCL, board.SDA)
         bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
-        bme280.sea_level_pressure = 1013.25
     except:
         print(1)
-i2c = I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
+bme280.sea_level_pressure = 1013.25
 
 # change this to match the location's pressure (hPa) at sea level
-bme280.sea_level_pressure = 1013.25
 if len(sys.argv) == 2:
     try:
         print(requests.post(host, data={"name":name, "temp":bme280.temperature, "humidity":bme280.humidity, "pressure":bme280.pressure, "altitude":bme280.altitude}))
