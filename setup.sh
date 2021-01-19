0798eb68cb64089b45a8825720349343a733d8ed
Elam Day <elam.day.friedland@gmail.com>
2021-01-18 20:38:44 -0800

    asd

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ if [ "$(id -u)" != "0" ]; then
 	echo "You must be the superuser to run this script :( try adding \"sudo\" at the begining of your command and rerun" >&2
 	exit 1
 fi
+error() { clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;}
 i2cDeviceExists() {
 	devs=$(i2cdetect -y 1 | sed 1d | sed 's/^....//' | sed 's/--//g')
 	# if [ -n "${devs// }" ]; then
