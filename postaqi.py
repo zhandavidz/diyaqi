562f4c3c522865f41f3c4f8516064a74859099f4
Elam Day <elam.day.friedland@gmail.com>
2021-01-18 19:46:47 -0800

    woho problems

--- postaqi.py
+++ postaqi.py
@@ -12,7 +12,12 @@ try:
     host = config.readline()
     name = config.readline()
 except Exception as e:
-    print(e)
+    try:
+        i2c = I2C(board.SCL, board.SDA)
+        bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+        bme280.sea_level_pressure = 1013.25
+    except:
+        print(1)
 i2c = I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
